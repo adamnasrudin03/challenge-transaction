0c2ff6de9332c56b9cc3f71b69de3749f64746b0
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-06 10:46:49 +0700

    init

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"adamnasrudin03/challenge-transaction/app/configs"
+	"adamnasrudin03/challenge-transaction/pkg/database"
+	"adamnasrudin03/challenge-transaction/pkg/helpers"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB = database.SetupDbConnection()
+)
+
+func main() {
+	defer database.CloseDbConnection(db)
+
+	router := gin.Default()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(cors.Default())
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, helpers.APIResponse("welcome its server", http.StatusOK, false, nil))
+	})
+
+	// Route here
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, helpers.APIResponse("page not found", http.StatusNotFound, true, nil))
+	})
+
+	config := configs.GetInstance()
+	listen := fmt.Sprintf(":%v", config.Appconfig.Port)
+	router.Run(listen)
+}
