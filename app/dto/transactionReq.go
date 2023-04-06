c578bb9e67819b19f1daa82bf24f071c3addbe6a
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-06 15:17:14 +0700

    fix validation struct

--- app/dto/transactionReq.go
+++ app/dto/transactionReq.go
@@ -5,12 +5,12 @@ import (
 )
 
 type TransactionReq struct {
-	RequestID uint64               ` json:"request_id"`
+	RequestID uint64               ` json:"request_id" validate:"required"`
 	Data      []entity.Transaction ` json:"data"`
 }
 
 type ParamTransactions struct {
-	Page   uint64 `json:"page" valid:"Required"`
-	Limit  uint64 `json:"limit" valid:"Required"`
+	Page   uint64 `json:"page" validate:"required"`
+	Limit  uint64 `json:"limit" validate:"required"`
 	Offset uint64 `json:"offset"`
 }
