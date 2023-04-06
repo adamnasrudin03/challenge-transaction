3a9549f4236ad61d626c1b6ed01ecb044c3f8878
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-06 14:32:46 +0700

    fix type data

--- app/entity/transaction.go
+++ app/entity/transaction.go
@@ -1,11 +1,9 @@
 package entity
 
-import "time"
-
 type Transaction struct {
-	ID        uint64    `gorm:"primary_key:auto_increment" json:"id"`
-	Customer  string    `gorm:"type:varchar(100);default:''" json:"customer"`
-	Quantity  uint64    `gorm:"type:bigint(20);default:0" json:"quantity"`
-	Price     float64   `gorm:"type:double;default:0" json:"price"`
-	Timestime time.Time `gorm:"type:DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP" json:"timestime"`
+	ID        uint64  `gorm:"primary_key:auto_increment" json:"id"`
+	Customer  string  `gorm:"type:varchar(100);" json:"customer"`
+	Quantity  uint64  `gorm:"default:0" json:"quantity"`
+	Price     float64 `gorm:"default:0" json:"price"`
+	Timestime string  `gorm:"type:varchar(100)" json:"timestime"`
 }
